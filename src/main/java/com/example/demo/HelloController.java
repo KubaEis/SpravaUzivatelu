655527fa6e72f9d7229b2490414a457c39ad1236
eisjak01 <eisjak01@gasos-ro.cz>
2026-01-08 12:25:53 +0100

    .

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.Optional;
+
+public class HelloController {
+    @FXML
+    private TextField nameField, nicknameField, phoneField;
+    @FXML
+    private ListView<User> listViewUsers;
+    @FXML
+    private Button addContactButton, deleteButton, updateButton;
+    @FXML
+    private final ObservableList<User> listUsers = FXCollections.observableArrayList();
+
+    @FXML
+    protected void initialize() {
+        listViewUsers.getItems().add(new User("Jan Novak","Honzi","888999111"));
+        listViewUsers.getItems().add(new User("Ondra Musil","Kokot","123456789"));
+    }
+
+    @FXML
+    protected void onAddContactButtonClick() {
+        if (!nameField.getText().isEmpty() && !nicknameField.getText().isEmpty() && !phoneField.getText().isEmpty()) {
+            listUsers.add(new User(nameField.getText(),nicknameField.getText(),phoneField.getText()));
+            listViewUsers.setItems(listUsers);
+        }else{
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Informace");
+            alert.setHeaderText("Dulezite upozorneni");
+            alert.setContentText("Pro pridani kontaktu je nutne vyplnit vsechna pole.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    protected void onDeleteButtonClick() {
+        User user = listViewUsers.getSelectionModel().getSelectedItem();
+        if (user != null) {
+            listViewUsers.getItems().remove(user);
+        }
+    }
+
+    @FXML
+    protected void onUpdateButtonClick() {
+        User user = listViewUsers.getSelectionModel().getSelectedItem();
+        if (user != null) {
+
+        }
+    }
+}
